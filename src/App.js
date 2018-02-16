461939d71a789e4a08ba2e32c31b799f6217602c
glowatsk <glowatsk@gmail.com>
2018-02-15 17:42:03 -0700

    Adding rank component, adding particle js background

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/rank/Rank.js';
 import 'tachyons';
+import Particles from 'react-particles-js';
+
+const particlesOption = {
+    particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className = 'particles'
+              params={particlesOption}
+            />
+
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
       </div>
     );
