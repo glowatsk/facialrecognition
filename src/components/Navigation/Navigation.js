e2dce9e46ce5582affe7a027ed03bb04875b5a47
glowatsk <glowatsk@gmail.com>
2018-02-20 12:05:57 -0700

    Creating registering process

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
-const Navigation = () => {
-    return (
+const Navigation = ({ onRouteChange, isSignedin }) => {
+        if (isSignedin) {
+            return(
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out </p>
+            <p 
+            onClick={() => onRouteChange('Signout')}
+            className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
-
-    );
+            );
+        } else {
+            return(
+                <div>
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p 
+            onClick={() => onRouteChange('Signin')}
+            className='f3 link dim black underline pa3 pointer'>Sign In</p>
+            <p 
+            onClick={() => onRouteChange('Register')}
+            className='f3 link dim black underline pa3 pointer'>Register</p>
+        </nav>
+        </div>
+            )
+        }
 }
 
 export default Navigation;
